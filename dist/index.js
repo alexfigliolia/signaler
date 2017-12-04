aa38c2f24a6ce1c6e31facdf0d50ab3025ef7baf
alexfigliolia <alexfigliolia@gmail.com>
2017-12-03 23:47:37 -0500

    update

--- dist/index.js
+++ dist/index.js
@@ -17,9 +17,8 @@ var _clients = require('./clients');
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var server = new _hapi2.default.Server();
-var port = process.env.PORT || 9000;
 
-server.connection({ 'host': 'localhost', 'port': port });
+server.connection({ 'host': 'localhost', 'port': 80 });
 
 var io = (0, _socket2.default)(server.listener, {
   'reconnect': true,
