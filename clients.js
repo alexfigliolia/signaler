9df170bf389647901fe09561afd31902feba0684
alexfigliolia <alexfigliolia@gmail.com>
2017-12-03 23:08:00 -0500

    upload

---
+++ clients.js
@@ -0,0 +1,27 @@
+export const clients = {};
+
+export const addClient = (clientID, socket) => {
+  if(clients[clientID]) delete clients[clientID];
+  clients[clientID] = socket;
+  // console.log(Object.keys(clients));
+}
+
+export const removeClient = (clientID) => {
+  delete clients[clientID];
+  // console.log(Object.keys(clients));
+}
+
+export const generateClientId = (key) => {
+  let clientId = randomId();
+  if (!clients[clientId]) {
+    return clientId;
+  }
+  while (!!clients[clientId]) {
+    clientId = randomId();
+  }
+  return clientId;
+};
+
+const randomId = () => {
+  return (Math.random().toString(36) + '0000000000000000000').substr(2, 16);
+};
---
+++ server.js
@@ -0,0 +1,57 @@
+import Hapi from 'hapi'
+import socketio from 'socket.io';
+import url from 'url';
+import { clients, addClient, removeClient, generateClientId } from './clients';
+
+const server = new Hapi.Server();
+
+server.connection({ 'host': 'localhost', 'port': 9000 });
+
+const io = socketio(
+  server.listener,
+  {
+    'reconnect': true,
+    'reconnection delay': 500,
+    'max reconnection attempts': 99999,
+    'secure': true,
+    'sync disconnect on unload': true
+  }
+);
+
+// When a socket connects, set up the specific listeners we will use.
+io.on('connection', (socket) => {
+
+  socket.on('connected', (userdata) => {
+    const uniqueID = generateClientId();
+    addClient(uniqueID, socket);
+    io.to(socket.id).emit('uniqueID', uniqueID);
+    socket.piperChatID = uniqueID;
+  });
+
+  socket.on('candidate', (candidate) => {
+    io.to(clients[candidate.to].id).emit('candidate', candidate.candidate);
+  });
+
+  socket.on('accepted', (user) => {
+    io.to(clients[user.to].id).emit('accepted', user.from);
+  });
+
+  socket.on('offer', (offer) => {
+    io.to(clients[offer.to].id).emit('offer', {offer: offer.offer, from: offer.from});
+  });
+
+  socket.on('answer', (answer) => {
+    io.to(clients[answer.to].id).emit('answer', answer.answer);
+  });
+
+  socket.on('disconnect', () => {
+  	// console.log('A user disconnected');
+    removeClient(socket.piperChatID);
+    delete socket.piperChatID;
+ 	});
+});
+
+// Start the server
+server.start(() => {
+  // console.log('Server running at:', server.info.uri);
+});
