2a13ee57748ab99798d4868f512f74e63813a4b3
alexfigliolia <alexfigliolia@gmail.com>
2017-12-04 00:40:37 -0500

    update

--- dist/index.js
+++ dist/index.js
@@ -1,44 +1,5 @@
 'use strict';
 
-var start = function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            _context.prev = 0;
-            _context.next = 3;
-            return server.start();
-
-          case 3:
-            _context.next = 9;
-            break;
-
-          case 5:
-            _context.prev = 5;
-            _context.t0 = _context['catch'](0);
-
-            console.log(_context.t0);
-            process.exit(1);
-
-          case 9:
-            console.log('Server running at:', server.info.uri);
-
-          case 10:
-          case 'end':
-            return _context.stop();
-        }
-      }
-    }, _callee, this, [[0, 5]]);
-  }));
-
-  return function start() {
-    return _ref.apply(this, arguments);
-  };
-}();
-
-require('babel-polyfill');
-
 var _hapi = require('hapi');
 
 var _hapi2 = _interopRequireDefault(_hapi);
@@ -55,9 +16,17 @@ var _clients = require('./clients');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+var server = new _hapi2.default.Server();
 
-var server = new _hapi2.default.Server({ 'host': 'localhost', 'port': 9000 });
+server.connection({ 'host': 'localhost', 'port': 9000 });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function handler(request, reply) {
+    reply('hello!');
+  }
+});
 
 var io = (0, _socket2.default)(server.listener, {
   'reconnect': true,
@@ -100,6 +69,7 @@ io.on('connection', function (socket) {
   });
 });
 
-;
-
-start();
+// Start the server
+server.start(function () {
+  console.log('Server running at:', server.info.uri);
+});
